937e69df45a2facd01cd996b331011dce14a598b
AllenJoeG <allen.joeg@gmail.com>
2021-11-19 11:39:58 -0600

    placeholder CRUD routes on clientside

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,38 @@
-$(document).onReady(function(){
+$(document).ready(function(){
   console.log('jQuery: SCV Ready')
 });
+
+function loadClickListeners() {
+  //listeners for buttons
+};
+
+// function renderToDom(dbRows){
+  
+// };
+
+//GET ROUTE
+function getTasksAndRender(){
+  //fetch dbRows from the database through the server!
+  //AJAX
+  //select element, loop through dbRows, append
+};
+
+//POST ROUTE
+function submitTaskInformation(){
+  //package up and send newTaskObject to server for database
+};
+
+//PUT ROUTE
+function markInProgress(){
+  //AJAX and capture ID
+};
+
+//PUT ROUTE
+function markComplete(){
+  //AJAX and capture ID
+}
+
+//DELETE ROUTE
+function deleteTask(){
+  //AJAX and capture ID
+}
